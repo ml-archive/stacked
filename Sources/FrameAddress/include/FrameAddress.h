8bc87fab32607f14c5ab7daaaca7bb45a195088d
Brett Toomey <brettcwx@gmail.com>
2017-02-10 19:21:07 -0800

    Wrote another stack trace printer in C

--- Sources/FrameAddress/include/FrameAddress.h
+++ Sources/FrameAddress/include/FrameAddress.h
@@ -1,33 +1,6 @@
-//
-//  FrameAddress.h
-//  CwlUtils
-//
-//  Created by Matt Gallagher on 2016/02/26.
-//  Copyright © 2016 Matt Gallagher ( http://cocoawithlove.com ). All rights reserved.
-//
-//  Permission to use, copy, modify, and/or distribute this software for any
-//  purpose with or without fee is hereby granted, provided that the above
-//  copyright notice and this permission notice appear in all copies.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-//  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-//  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-//  SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-//  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-//  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
-//  IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-//
+#ifndef H_FRAME_ADDRESS
+#define H_FRAME_ADDRESS
 
-#include <sys/types.h>
-#include <stdint.h>
-#define _GNU_SOURCE
-#define __USE_GNU
-#include <dlfcn.h>
-#include <pthread.h>
+char **get_stack_trace(int buffer_size, int *result_count);
 
-#ifndef FrameAddress_h
-#define FrameAddress_h
-
-uintptr_t frame_address();
-
-#endif /* FrameAddress_h */
+#endif
