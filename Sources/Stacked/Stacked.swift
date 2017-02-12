85451f8c91f8494b4f0ed99003893e3d59efc2a9
Brett Toomey <brettcwx@gmail.com>
2017-02-11 19:21:29 -0800

    Testing demangling code

--- Sources/Stacked/Stacked.swift
+++ Sources/Stacked/Stacked.swift
@@ -1,4 +1,11 @@
 import FrameAddress
+import Foundation
+
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
 
 public struct FrameAddress {
     public static func getStackTrace() -> [String] {
@@ -14,9 +21,46 @@ public struct FrameAddress {
                 break
             }
             
-            result.append(String(cString: cString))
+            var components = String(cString: cString).components(separatedBy: " ")
+            for (index, component) in components.enumerated() {
+                if component.hasPrefix("_T") {
+                    components[index] = _stdlib_demangleName(component)
+                }
+            }
+            
+            result.append(components.joined(separator: " "))
         }
         
         return result
+    }
+}
+
+@_silgen_name("swift_demangle")
+public
+func _stdlib_demangleImpl(
+    mangledName: UnsafePointer<CChar>?,
+    mangledNameLength: UInt,
+    outputBuffer: UnsafeMutablePointer<CChar>?,
+    outputBufferSize: UnsafeMutablePointer<UInt>?,
+    flags: UInt32
+    ) -> UnsafeMutablePointer<CChar>?
+
+internal func _stdlib_demangleName(_ mangledName: String) -> String {
+    return mangledName.utf8CString.withUnsafeBufferPointer {
+        (mangledNameUTF8CStr) in
+        
+        let demangledNamePtr = _stdlib_demangleImpl(
+            mangledName: mangledNameUTF8CStr.baseAddress,
+            mangledNameLength: UInt(mangledNameUTF8CStr.count - 1),
+            outputBuffer: nil,
+            outputBufferSize: nil,
+            flags: 0)
+        
+        if let demangledNamePtr = demangledNamePtr {
+            let demangledName = String(cString: demangledNamePtr)
+            free(demangledNamePtr)
+            return demangledName
+        }
+        return mangledName
     }
 }
