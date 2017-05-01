73c617c7a011d29168a6b25e58b6264969728996
Brett R. Toomey <brettcwx@gmail.com>
2017-05-01 13:47:38 +0200

    Cleaned up stacktrace printing

--- Sources/FrameAddress/FrameAddress.c
+++ Sources/FrameAddress/FrameAddress.c
@@ -12,41 +12,34 @@
 char ** get_symbols_for_backtrace(void * const *buffer, int size) {
     int i;
     char ** result;
-    char ** ptrs;
-    intptr_t strs;
-    
+
     Dl_info *info = calloc(size, sizeof(Dl_info));
     if (info == NULL) return NULL;
     
-    size_t totalBytes = sizeof(char *) * size;
-    
-    for (i = 0; i <= size; ++i) {
-        dladdr(buffer[i], &info[i]);
-        if (info[i].dli_sname) totalBytes += (strlen(info[i].dli_sname) + 1);
-    }
-    
-    result = malloc(totalBytes);
+    result = malloc(sizeof(char *) * size);
     if (result == NULL) {
         free(info);
         return NULL;
     }
     
-    ptrs = result;
-    strs = ((intptr_t)result) + sizeof(char *) * size;
-    
-    for (i = 0; i < size; ++i) {
-        ptrs[i] = (char *)strs;
-        strs += snprintf((char *) strs, totalBytes, "%s", info[i].dli_sname) + 1;
+    for (i = 0; i <= size; ++i) {
+        dladdr(buffer[i], &info[i]);
+        
+        char *name = "???";
+        if (info[i].dli_sname) {
+            name = (char *)info[i].dli_sname;
+        }
+        
+        result[i] = strdup(name);
     }
     
-    //free(info);
+    free(info);
     return result;
 }
 
 char **get_stack_trace(int buffer_size, int *result_count) {
     void *buffer[buffer_size];
-    char **strings;
-
     *result_count = backtrace(buffer, buffer_size);
-    return get_symbols_for_backtrace(buffer, buffer_size);
+    
+    return get_symbols_for_backtrace(buffer, *result_count);
 }
