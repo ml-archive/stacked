3c26c9a55673b9b9a79c9556f65663738635a600
Steffen D. Sommer <steffendsommer@icloud.com>
2018-01-11 15:49:03 +0100

    More debug

--- Tests/StackedTests/StackedTests.swift
+++ Tests/StackedTests/StackedTests.swift
@@ -5,20 +5,8 @@ class StackedTests: XCTestCase {
     static var allTests = [
         ("testExample", testExample)
     ]
-    
-    func anExample() throws -> [String] {
-        return anotherExample()
-    }
-    
-    func anotherExample() -> [String] {
-        return FrameAddress.getStackTrace()
-    }
 
     func testExample() {
         XCTAssert(true)
-        let stacktrace = try! anExample()
-        stacktrace.forEach {
-            print($0)
-        }
     }
 }
