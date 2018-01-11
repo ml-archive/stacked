0ac685ee9b6f630b6f5ae8905f1f351fec729f9e
Steffen D. Sommer <steffendsommer@icloud.com>
2018-01-11 12:23:41 +0100

    Use CStack 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,15 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Stacked",
     dependencies: [
-        .Package(url: "https://github.com/nodes-vapor/cstack.git", majorVersion: 0)
+        .package(url: "https://github.com/nodes-vapor/cstack.git", .upToNextMajor(from: "1.0.0")),
+    ],
+    targets: [
+        .target(name: "Stacked", dependencies: []),
+        .testTarget(name: "StackedTests", dependencies: ["Stacked"]),
     ]
 )
